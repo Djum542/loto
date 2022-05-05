94fcf2f6d2240f5fac18da80addf0c03b7715663
Djum542 <djum54022@gmail.com>
2022-05-05 19:13:18 +0700

    loto

--- app/src/main/java/com/example/lotoliveshow/MainActivity.java
+++ app/src/main/java/com/example/lotoliveshow/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.lotoliveshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView nTvloto, nTveaseout, nTveaseInout;
+Button mBtLai, mBtRao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
